5534be1e91af30e75b04e338408aae3d3775e9db
Angelica-AJ <angie.aj03@yahoo.com>
2025-03-25 02:05:26 -0400

    updatesss

---
+++ jquery/main.js
@@ -0,0 +1,20 @@
+$(".dark-btn").click(function(){
+    $(".square").toggleClass("dark");
+    $("body").toggleClass("dark");
+  }
+);
+
+$(".spin-btn").click(
+    function(){
+        $(".square").toggleClass("spin");
+    }
+);
+
+$(".reveal-btn").click(
+    function(){
+        $(".chair").addClass("reveal");
+        $(".reveal-btn").hide();
+    }
+);
+
+ $( ".draggable" ).draggable();
